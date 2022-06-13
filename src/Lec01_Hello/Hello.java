5085b838c32c75da78eac47173835ca064e6bff5
Nappy <wjddmlgud829@gmail.com>
2022-06-13 17:30:34 +0900

    hiyo!

---
+++ src/Lec01_Hello/Hello.java
@@ -0,0 +1,13 @@
+package Lec01_Hello;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello Java!");
+
+        System.out.println("안녕하세요");
+        System.out.println("자바");
+    }
+
+}
